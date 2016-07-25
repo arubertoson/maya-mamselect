8a271b9e7ce8841f6f673cc433d9a078c623a441
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-07-25 16:03:56 +0200

    0.1.2

--- mamselect/__init__.py
+++ mamselect/__init__.py
@@ -1,6 +1,8 @@
 
 
 # from mamselect import masks
-from mamselect import masks, mesh, pattern
+from mamselect import masks, mesh, pattern, isolate, subd
 from mamselect.coplanar import coplanar
 from mamselect.fill import fill
+
+__version__ = '0.1.2'
