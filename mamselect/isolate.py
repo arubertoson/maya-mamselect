6947f95786f9da0859f11347c2786c4ca27f827d
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2016-08-07 02:30:09 +0200

    refactored to fix various issues.
    Now handles isolate selection as intended. No components can be isolated and isolation won't break when user has a component selected.

--- mamselect/isolate.py
+++ mamselect/isolate.py
@@ -2,7 +2,8 @@
 Improvement to Mayas isolate select tool.
 
 I wanted more control when isolating and realized that I was re isolating objects
-a lot after having initially isolated an object. A lot of functionality was not there when this was created, such as auto adding new objects on creation, meaning
+a lot after having initially isolated an object. A lot of functionality was not
+there when this was created, such as auto adding new objects on creation, meaning
 some functionality is obsolete.
 
 To note, this isolating system works only on objects. This means you can't isolate
@@ -16,6 +17,7 @@ from PySide.QtCore import QTimer
 from maya import cmds
 from maya.OpenMaya import MEventMessage
 
+import mampy
 
 logger = logging.getLogger(__name__)
 
@@ -26,19 +28,79 @@ TIMER_SET = False
 SELECT_CHANGE_EVENT = None
 
 
+def is_component(selected):
+    return any(i.is_valid() for i in selected.itercomps())
+
+
+def get_selected_transforms(selection):
+    return [str(i.get_transform())[1:] for i in selection.iterdags()]
+
+
+def get_clean_dagpaths(dagpaths):
+    return [str(i)[1:] for i in dagpaths.iterdags()]
+
+
+def get_clean_list_object(objects):
+    if is_component(objects):
+        return get_selected_transforms(objects)
+    return get_clean_dagpaths(objects)
+
+
+def get_selected_objects():
+    return get_clean_list_object(mampy.selected())
+
+
+def get_isolate_set_name():
+    return cmds.isolateSelect(get_active_panel(), q=True, viewObjects=True)
+
+
 def isolate_new_objects():
     objs = get_selected_objects()
     logger.debug('changed selection: {}'.format(objs))
     if objs:
-        if not any(i in objs[0] for i in ['.f', '.vtx', '.e', '.map']):
-            try:
-                cmds.sets(objs, include=get_isolate_set_name())
-            except TypeError:
-                pass
+        try:
+            cmds.sets(objs, include=get_isolate_set_name())
+        except TypeError:
+            pass
 
 
-def get_isolate_state():
+def get_active_panel():
+    return cmds.getPanel(withFocus=True)
+
+
+def is_isolated():
     return cmds.isolateSelect(get_active_panel(), q=True, state=True)
+
+
+def set_isolate_set(selection):
+    set_name = get_isolate_set_name()
+
+    # Unhilite
+    isoset = cmds.ls(hl=True)
+    for obj in isoset:
+        if obj in selection:
+            isoset.remove(obj)
+    cmds.hilite(isoset, toggle=True)
+
+    cmds.sets(clear=set_name)
+    cmds.sets(selection, include=set_name)
+
+
+def set_isolate_selected_off_or_update():
+    global SELECT_CHANGE_EVENT
+    isoset = cmds.sets(get_isolate_set_name(), q=True)
+    selset = get_selected_objects()
+
+    if isoset:
+        if set(isoset) == set(selset) or not selset:
+            try:
+                MEventMessage.removeCallback(SELECT_CHANGE_EVENT)
+                SELECT_CHANGE_EVENT = None
+            except RuntimeError:
+                pass
+            cmds.isolateSelect(get_active_panel(), state=False)
+        else:
+            set_isolate_set(selset)
 
 
 def on_selection_changed(*args):
@@ -54,66 +116,27 @@ def create_select_change_event():
     global SELECT_CHANGE_EVENT
     if SELECT_CHANGE_EVENT:
         return True
-    event = 'SelectionChanged'
-    callback = on_selection_changed
-    SELECT_CHANGE_EVENT = MEventMessage.addEventCallback(event, callback)
-
-
-def get_active_panel():
-    return cmds.getPanel(withFocus=True)
-
-
-def get_isolate_set_name():
-    return cmds.isolateSelect(get_active_panel(), q=True, viewObjects=True)
-
-
-def update_panel():
-    try:
-        cmds.sets(clear=get_isolate_set_name())
-    except TypeError:
-        pass
-    finally:
-        isolate_new_objects()
+    SELECT_CHANGE_EVENT = MEventMessage.addEventCallback('SelectionChanged',
+                                                         on_selection_changed)
 
 
 def set_isolate_selected_on():
-    if not get_selected_objects():
+    sel = get_selected_objects()
+    if not sel:
         return logger.warn('Nothing selected, ignoring isolate.')
+
     cmds.isolateSelect(get_active_panel(), state=True)
-    cmds.isolateSelect(get_active_panel(), addSelected=True)
+    set_isolate_set(sel)
     create_select_change_event()
-
-
-def get_selected_objects():
-    return cmds.ls(sl=True) + cmds.ls(hl=True)
-
-
-def set_isolate_selected_off_or_update():
-    global SELECT_CHANGE_EVENT
-    isoset = cmds.sets(get_isolate_set_name(), q=True)
-    selset = get_selected_objects()
-
-    if isoset:
-        if set(isoset) == set(selset) or not selset:
-            # Cleanup
-            try:
-                MEventMessage.removeCallback(SELECT_CHANGE_EVENT)
-                SELECT_CHANGE_EVENT = None
-            except RuntimeError:
-                pass
-            cmds.isolateSelect(get_active_panel(), state=False)
-        else:
-            update_panel()
 
 
 def toggle():
     global TIMER_SET
-    # Connect timer object to isolate new function.
     if not TIMER_SET:
         TIMER.timeout.connect(isolate_new_objects)
         TIMER_SET = True
 
-    if not get_isolate_state():
+    if not is_isolated():
         return set_isolate_selected_on()
     set_isolate_selected_off_or_update()
 
