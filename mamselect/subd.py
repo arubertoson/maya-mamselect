70ab83b20414d914d4022fd7dcf7b9a4905c09f1
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2016-08-21 18:02:17 +0200

    Refactored to work with new mampy while fixing minor bugs

--- mamselect/subd.py
+++ mamselect/subd.py
@@ -11,47 +11,57 @@ even the subdivision levels.
 from functools import partial
 
 from maya import cmds
+from maya.api.OpenMaya import MFn
 
 import mampy
-from mampy._old.containers import SelectionList
+from mampy.core.selectionlist import DagpathList
 
 
-def get_shape_from_object(objects):
-    selected = SelectionList()
-    for mesh in objects.iterdags():
-        shape = mesh.get_shape()
-        if shape is None:
-            continue
-        selected.append(shape)
-    return selected
+def find_matching_in_hilited(objects):
+    hilited = mampy.daglist(hl=True, dag=True, type='mesh')
+    if not hilited:
+        return objects
 
-
-def merge_with_hilited(objects):
-    hilited = mampy.ls(hl=True, dag=True, type='mesh')
-    for h in hilited:
-        if h in objects:
-            continue
-        objects.append(h)
+    for obj in objects:
+        parent = str(obj).lstrip('|').split('|')[0]
+        for child in hilited:
+            if child == obj:
+                continue
+            child_name = str(child).lstrip('|').split('|')[0]
+            if parent == child_name:
+                objects.append(child)
     return objects
 
 
+Selected, Hierarchy, All = (0, 1, 2)
 def get_mesh_objects(mode):
     objects = {
-        'all': partial(mampy.ls, type='mesh'),
-        'hierarchy': partial(mampy.ls, sl=True, dag=True, type='mesh'),
-        'selected': partial(mampy.ls, sl=True),
+        All: partial(mampy.daglist, type='mesh'),
+        Hierarchy: partial(mampy.daglist, sl=True, dag=True, type='mesh'),
+        Selected: partial(mampy.daglist, sl=True),
     }[mode]()
-    if mode == 'all':
+    if mode == All:
         return objects
+    elif mode == Hierarchy and not objects:
+        objects = mampy.daglist()
 
-    # Get shape nodes from selected.
-    if mode == 'selected':
-        objects = get_shape_from_object(objects)
-    return merge_with_hilited(objects)
+    # Make subd toggle work when in component mode.
+    if mode == Hierarchy:
+        objects = find_matching_in_hilited(objects)
+    # Add hilited objects to existing list and sort out meshes.
+    # objects.extend(other)
+    mesh_objects = DagpathList()
+    for dag in objects:
+        if dag.type == MFn.kMesh:
+            mesh_objects.append(dag)
+        else:
+            if dag.shape.type == MFn.kMesh:
+                mesh_objects.append(dag.shape)
+    return mesh_objects
 
 
 def toggle(hierarchy=False):
-    meshes = get_mesh_objects('hierarchy' if hierarchy else 'selected')
+    meshes = get_mesh_objects(Hierarchy if hierarchy else Selected)
     for mesh in meshes:
         try:
             state = cmds.displaySmoothness(str(mesh), q=True, po=True).pop()
@@ -61,19 +71,15 @@ def toggle(hierarchy=False):
 
 
 def toggle_all(state):
-    meshes = get_mesh_objects('all')
+    meshes = get_mesh_objects(All)
     for mesh in meshes:
         cmds.displaySmoothness(str(mesh), po=3 if state else 0)
 
 
 def set_smooth_level(level=1, all=True, hierarchy=False):
     if all:
-        meshes = get_mesh_objects('all')
+        meshes = get_mesh_objects(All)
     else:
-        meshes = get_mesh_objects('hierarchy' if hierarchy else 'hierarchy')
+        meshes = get_mesh_objects(Hierarchy if hierarchy else Selected)
     for mesh in meshes.iterdags():
-        mesh['smoothLevel'] = mesh['smoothLevel'] + level
-
-
-if __name__ == '__main__':
-    set_smooth_level(-1)
+        mesh.attr['smoothLevel'] = mesh.attr['smoothLevel'] + level
